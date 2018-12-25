dd7fe22c77f9d015e791e9b614328bffb56b5ddc
Dima <angelov.dmitry@gmail.com>
2018-12-25 14:37:25 +0100

    sec2

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ function Square(props) {
         <button className="square" onClick={props.onClick}>
             {props.value}
         </button>
+        //some other line added in second
     );
 }
 
